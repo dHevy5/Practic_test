463d9752f4efabdfa8a9d85f978c77a40ebe860a
dHevy5 <160593982+dHevy5@users.noreply.github.com>
2024-02-26 15:14:02 +0400

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter first number: " << endl;
+    cin >> a;
+    cout << "Enter second number: " << endl;
+    cin >> b;
+
+    char x;
+
+    cout << "Enter the operatoin: " << endl;
+    cin >> x;
+    switch (x)
+    {
+    case '+':
+    {
+        double plus = a + b;
+        cout << "Solvece: " << plus;
+        break;
+    }
+    case '-':
+    {
+        double minus = a - b;
+        cout << "Solvece: " << minus;
+        break;
+    }
+    case '*':
+    {
+        double multiplus = a * b;
+        cout << "Solvece: " << multiplus;
+        break;
+    }
+    case '/':
+    {
+        double share = a / b;
+        cout << "Solvece: " << share;
+        break;
+    }
+
+    }
+    return 0;
+
+}
